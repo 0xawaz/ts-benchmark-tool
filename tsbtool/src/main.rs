6a74061de22b42a3073f9be0e91454b4e97f02d5
0xawaz <amina.waddiz@gmail.com>
2024-07-20 14:10:19 +0200

    add --help flags

--- tsbtool/src/main.rs
+++ tsbtool/src/main.rs
@@ -1,11 +1,12 @@
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(Debug, StructOpt)]
 struct Cli {
     /// Path to the CSV file
+    #[structopt(help = "Path to the CSV file")]
     csv_file: String,
     /// Number of workers
-    #[structopt(short, long, default_value = "5")]
+    #[structopt(short, long, default_value = "5", help = "Number of workers (default is 5)")]
     workers: usize,
 }
 
@@ -13,8 +14,8 @@ fn main() {
     let args = Cli::from_args();
 
     if let Err(err) = tsbtool::read_csv(&args.csv_file) {
-        eprintln!("Error reading CSV file: {}", err);
+        eprintln!("Error reading CSV file: {}. Please provide a valid CSV file and a valid path.", err);
     }
 
-    println!("Number of workers: {}", args.workers);
+    // println!("Number of workers: {}", args.workers);
 }
